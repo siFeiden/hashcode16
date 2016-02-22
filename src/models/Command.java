de3bc61e1b37e7a461f0cb0f44f7f8d690260aa3
Simon Feiden <si.feiden@gmail.com>
2016-02-22 16:43:14 +0100

    more flexible Command implementation

--- src/models/Command.java
+++ src/models/Command.java
@@ -1,30 +1,14 @@
 package models;
 
-public final class Command {
-    private final int droneId;
-    private final String command;
-    private final int warehouseId;
-    private final int productId;
-    private final int amount;
+public interface Command {
 
-    private Command(int droneId, String command, int warehouseId, int productId, int amount) {
-        this.droneId = droneId;
-        this.command = command;
-        this.warehouseId = warehouseId;
-        this.productId = productId;
-        this.amount = amount;
+    String format();
+
+    static Command load(int droneId, int warehouseId, int productId, int amount) {
+        return () -> String.format("%d L %d %d %d", droneId, warehouseId, productId, amount);
     }
 
-    public String format() {
-        return String.format("%d %s %d %d %d", droneId, command, warehouseId, productId, amount);
-    }
-
-
-    public static Command load(int droneId, int warehouseId, int productId, int amount) {
-        return new Command(droneId, "L", warehouseId, productId, amount);
-    }
-
-    public static Command deliver(int droneId, int orderId, int productId, int amount) {
-        return new Command(droneId, "D", orderId, productId, amount);
+    static Command deliver(int droneId, int orderId, int productId, int amount) {
+        return () -> String.format("%d D %d %d %d", droneId, orderId, productId, amount);
     }
 }
